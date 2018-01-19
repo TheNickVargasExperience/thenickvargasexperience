dbadfe27f08ee1628b7a2148c4d640475eab72e0
Simon <sjellerbusch15@gmail.com>
2018-01-18 22:41:56 -0600

    Added Safari Pic, and fixed pics mobile size

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,11 +3,23 @@ $(document).ready(function(){
 
   autoplaySpeed: 3000,
   dots: true,
-  arrows: false,
+  arrows: true,
   infinite: true,
   speed: 800,
   slidesToShow: 1,
   adaptiveWidth: true,
   arrows: true
+});
+$('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 650, 'swing', function () {
+        window.location.hash = target;
+    });
 });
 });
