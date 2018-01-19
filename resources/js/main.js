a5c1f3db9cc8581a9f4a73013629bcfb2924a2f9
Simon <sjellerbusch15@gmail.com>
2018-01-18 20:58:12 -0600

    Re-did site, switched to Slick Carosel

---
+++ resources/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.slideShow').slick({
+
+  autoplaySpeed: 3000,
+  dots: true,
+  arrows: false,
+  infinite: true,
+  speed: 800,
+  slidesToShow: 1,
+  adaptiveWidth: true,
+  arrows: true
+});
+});
